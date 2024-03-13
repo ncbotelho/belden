948e7172e5be58833f03b2be3e3a5902a2585039
ncbotelho <149608283+ncbotelho@users.noreply.github.com>
2024-03-13 20:37:16 -0300

    Update homecard.js

--- homecard.js
+++ homecard.js
@@ -6,10 +6,10 @@
     const footerBtn = document.createElement("ui5-button");
     const userId = document.querySelector("meta[name='userId']").getAttribute("content");
     // set attributes
-    dialog.setAttribute("header-text", "Third Party JavaScript");
+    dialog.setAttribute("header-text", "New Hire Survey");
     footer.setAttribute("slot", "footer");
     // content
-    content.innerHTML = `Hi ${userId}. This dialog was created by third party JS file`;
+    content.innerHTML = `<p>Your experience matters to us, and we'd love to hear your thoughts on our onboarding process. Please take a moment to complete our brief survey. Your participation and feedback will help us improve our processes.</p><p><a href="https://beldenex.sjc1.qualtrics.com/jfe/form/SV_8oJYB49V73q98gu?${userId}">Take the survey</a></p>`;
     content.style.padding = "1rem";
     // footer btn
     footerBtn.onclick = () => dialog.close();
