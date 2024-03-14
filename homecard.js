103353c600231843385312c2d57333adef606f30
ncbotelho <149608283+ncbotelho@users.noreply.github.com>
2024-03-13 21:42:32 -0300

    Update homecard.js

--- homecard.js
+++ homecard.js
@@ -9,7 +9,7 @@
     dialog.setAttribute("header-text", "New Hire Survey");
     footer.setAttribute("slot", "footer");
     // content
-    content.innerHTML = `<p>Your experience matters to us, and we'd love to hear your thoughts on our onboarding process. Please take a moment to complete our brief survey. Your participation and feedback will help us improve our processes.</p><p><a href="https://beldenex.sjc1.qualtrics.com/jfe/form/SV_8oJYB49V73q98gu?userId=${userId}" target="_blank">Take the survey</a></p>`;
+    content.innerHTML = `<p>Your experience matters to us, and we'd love to hear your thoughts on our onboarding process. Please take a moment to complete our brief survey. Your participation and feedback will help us improve our processes.</p><p><a href="https://beldenex.sjc1.qualtrics.com/jfe/form/SV_6ssif3JaAAGH61U?Q_TS_ID=TS_9w8dwnwQEzU2rwA&Q_TS_RS=Self&Q_EE_ANON=1&_=1&userId=${userId}" target="_blank">Take the survey</a></p>`;
     content.style.padding = "1rem";
     // footer btn
     footerBtn.onclick = () => dialog.close();
